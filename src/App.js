1daf0647ca785c4630bd0768ab143e8d4b5388a6
vv_tomilin <backatom1@gmail.com>
2021-05-29 13:39:23 +0500

    Lesson #5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import axios from 'axios';
+
 import { Home, Cart } from './pages';
 import { Header } from './components';
 
 import { Route } from 'react-router';
 
 function App() {
+
+  const [pizzas, setPizzas] = React.useState([]);
+
+  //% componentDidMount()
+  //* с добавлением "[]" вторым параметром в "useEffect()" 
+  //* работает как "componentDidMount()"
+  React.useEffect(() => {
+    // fetch("http://localhost:3000/db.json")
+    //   .then((resp) => {
+    //     return resp.json()
+    //   })
+    //   .then(json => {
+    //     return setPizzas(json.pizzas);
+    //   })
+
+    axios.get("http://localhost:3000/db.json")
+      .then(({ data }) => {
+        setPizzas(data.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path='/' component={Home} exact />
+        <Route path='/' render={() => <Home items={pizzas} />} exact />
         <Route path='/cart' component={Cart} exact />
 
       </div>
